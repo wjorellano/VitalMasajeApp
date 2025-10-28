0800e24dd060836eb9f8bdae3482ae8a92158b84
willman orellano <wilmanjunior2001@gmail.com>
2025-10-28 18:50:24 -0500

    First commit

---
+++ App.jsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar,Text, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+
+function App() {
+
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Welcome to the React Native App!</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+export default App;
